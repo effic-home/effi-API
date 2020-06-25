a0bff8eb92bffce129aec1b4ff8096a9510bb9a9
Cyndie <lefebvre.cyndie@hotmail.fr>
2020-06-25 10:28:14 +0200

    Ajout de variables d'environnements pour Docker

--- db/connexion.js
+++ db/connexion.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
-var connexion = require('./example.secret').connexion;
+//var connexion = require('./example.secret').connexion;
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : connexion.host,
-    user     : connexion.user,
-    password : connexion.password,
-    database : connexion.database,
+    host     : process.env.HOST,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE,
 });
 connection.connect(function(err) {
     if (err) throw err;
     else
-        console.log('Connecté à '+connexion.database);
+        console.log('Connecté à '+ connection.database);
 });
 
 module.exports = connection;
